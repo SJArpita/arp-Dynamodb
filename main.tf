f831d3e13456dcfef55a38742fd9fca380b91874
SJArpita <rsheea12@gmail.com>
2025-02-09 23:43:07 +0800

    Add files via upload

--- main.tf
+++ main.tf
@@ -1,11 +1,41 @@
-module "web_app" {
- source = "./modules/web_app"
+resource "aws_dynamodb_table" "bookinventory" {
+  name           = "${var.prefix}-bookinventory"
+  billing_mode   = "PAY_PER_REQUEST" # On-demand billing
+  hash_key       = "ISBN" # Partition Key
+  range_key      = "Genre" # Sort Key
 
- name_prefix = "arpita" 
+  attribute {
+    name = "ISBN" 
+    type = "S" # String
+  }
+  attribute {
+    name = "Genre"
+    type = "S" # String
+  }
 
- instance_type  = "t2.micro"
- instance_count = 2
- 
- vpc_id        = "vpc-012814271f30b4442"
- public_subnet = true
+  # Set lifecycle rules to prevent accidental replacement of existing resources  
+ /*  lifecycle {
+    prevent_destroy = true
+  } */
+
+  tags = {
+    Environment = "Dev"
+    Application = "BookInventory"
+    CreatedBy   = var.createdByTerraform
+  }
+}
+
+# Option to Force Replace
+# If you want to delete and recreate the table (WARNING: this deletes existing data), 
+# use the terraform destroy command:
+#   terraform destroy -target=aws_dynamodb_table.bookinventory
+# Then, reapply the configuration:
+#   terraform apply
+
+
+output "dynamodb_table_arn" {
+  value = aws_dynamodb_table.bookinventory.arn
+}
+output "dynamodb_table_name" {
+  value = aws_dynamodb_table.bookinventory.name
 }
